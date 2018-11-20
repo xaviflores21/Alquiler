1a5f0ff67987450b9cf0e9cb14a8cbe7cc28cb10
Javier Flores <willams_21@hotmail.com>
2018-11-20 12:49:58 -0400

    cvasdgf

---
+++ app/src/main/java/com/xavi/alquiler/Detalle_Explorer_Activity.java
@@ -0,0 +1,13 @@
+package com.xavi.alquiler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detalle_Explorer_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle__explorer_);
+    }
+}
